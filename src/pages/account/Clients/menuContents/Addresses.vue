6ee668726bc5ffd11358ae450f9de139362e6393
emekajoseph <josephiwuji@gmail.com>
2025-04-27 01:18:42 +0100

    General and Addresses Menu

--- src/pages/account/Clients/menuContents/Addresses.vue
+++ src/pages/account/Clients/menuContents/Addresses.vue
@@ -1,20 +1,127 @@
 <template>
     <div class="card h-100 border-0 rounded-4">
-        <div class="card-body">
-            {{ clientDetails }}
+        <div class="card-header bg-transparent border-0">
+            <span v-if="!isAddingNew" class="fw-500">Addresses</span>
+            <span v-else>
+                <i @click="isAddingNew = false" class="bi bi-arrow-left cursor-pointer"></i>
+                Add New Address
+            </span>
+            <button v-if="!isAddingNew" @click="isAddingNew = true"
+                class="btn btn-sm btn-outline-dark float-end rounded-4 ">
+                <i class="bi bi-plus"></i> Add New Address
+            </button>
+        </div>
+        <div v-if="!isAddingNew" class="card-body">
+            <EasyDataTable show-index alternating :headers="headers" :items="sampleData.ClientAddresses"
+                buttons-pagination>
+                <template #header="header">
+                    <span>{{ header.text == '#' ? 'S/N' : header.text }}</span>
+                </template>
+
+                <template #item-type="item">
+                    <span class="text-muted text-center text-uppercase">{{ item.type }}</span>
+                </template>
+
+                <template #item-action="item">
+                    <a @click="showDetails(item)" href="#">Details</a>
+                </template>
+            </EasyDataTable>
+        </div>
+
+        <div v-else class="card-body">
+            <div class="row g-3 small">
+                <div class="col-md-4">
+                    <div class="small text-muted">Birth Country</div>
+                    <select class="form-select ">
+                        <option selected>Select--</option>
+                    </select>
+                </div>
+                <div class="col-md-4">
+                    <div class="small text-muted">Property Phpne Number</div>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Building Name</div>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Line</div>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">City</div>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">State</div>
+                    <select class="form-select ">
+                        <option selected>Select--</option>
+                    </select>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">From</div>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">After</div>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Type</div>
+                    <select class="form-select ">
+                        <option selected>Select--</option>
+                    </select>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Postal Code</div>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div v-if="isAddingNew" class="col-12">
+                    <button class="btn btn-theme rounded-4 float-end">
+                        Add New Address
+                    </button>
+                    <button @click="isAddingNew = false" class="btn btn-outline-dark me-2 rounded-4 float-end">
+                        Cancel
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
-
 
 
 </template>
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import { useClientsStore } from '../clientsStore';
-
-
-
+import { ref } from 'vue';
+import sampleData from '@/stores/sample_data.json'
+import type { Header, Item } from 'vue3-easy-data-table';
 
 const clientsStore = useClientsStore()
 const { clientDetails } = storeToRefs(clientsStore)
+
+const headers = ref<Header[]>([
+    { text: 'Birth Country', value: 'country', sortable: true },
+    { text: 'State', value: 'state', sortable: true },
+    { text: 'City', value: 'city', sortable: true },
+    { text: 'Type', value: 'type', sortable: true },
+    { text: 'Action', value: 'action' },
+])
+
+
+const isAddingNew = ref<boolean>(false)
+
+
+
+function showDetails(item: any) {
+}
 </script>
--- src/pages/account/Clients/menuContents/General.vue
+++ src/pages/account/Clients/menuContents/General.vue
@@ -1,15 +1,133 @@
 <template>
     <div class="card h-100 border-0 rounded-4">
         <div class="card-header bg-transparent border-0">
-            <button class="btn btn-sm btn-outline-dark float-end rounded-4 ">
+            <span v-if="!isEditing" class="fw-500">General</span>
+            <span v-else>
+                <i @click="isEditing = false" class="bi bi-arrow-left cursor-pointer"></i>
+                Edit Clients Details
+            </span>
+            <button v-if="!isEditing" @click="isEditing = true"
+                class="btn btn-sm btn-outline-dark float-end rounded-4 ">
                 <i class="bi bi-pencil-fill"></i> Edit
             </button>
         </div>
         <div class="card-body">
-            <div class="d-flex flex-wrap gap-2">
-                <div>
+            <!-- <div class="d-flex flex-wrap gap-2"> -->
+            <div class="row g-3 small">
+                <div class="col-md-4">
                     <div class="small text-muted">First Name</div>
-                    {{ clientDetails }}
+                    <span v-if="!isEditing"> {{ clientDetails?.name }}</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Middle Name</div>
+                    <span v-if="!isEditing">Nill</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Last Name</div>
+                    <span v-if="!isEditing">Robertson</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Email Address</div>
+                    <span v-if="!isEditing">{{ clientDetails?.email }}</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Date of birth</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Nationality</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Mobile</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+                <div class="col-md-4">
+                    <div class="small text-muted">Telephone</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+                <div class="col-md-4">
+                    <div class="small text-muted">Birth Country</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <select v-else class="form-select ">
+                        <option selected>Select--</option>
+                    </select>
+                </div>
+                <div class="col-md-4">
+                    <div class="small text-muted">Gender</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <select v-else class="form-select ">
+                        <option selected>Select--</option>
+                    </select>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Join Date</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+                <div class="col-md-4">
+                    <div class="small text-muted">Created On</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+                <div class="col-md-4">
+                    <div class="small text-muted">SSN Number (US Only)</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Social Insurance Number</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">National Identity Number</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">Tax Identification Number</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">External ID</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="small text-muted">CLient ID</div>
+                    <span v-if="!isEditing">Nil</span>
+                    <input v-else type="text" class="form-control">
+                </div>
+
+                <div v-if="isEditing" class="col-12">
+                    <button class="btn btn-theme rounded-4 float-end">
+                        Edit Client
+                    </button>
+                    <button @click="isEditing = false" class="btn btn-outline-dark me-2 rounded-4 float-end">
+                        Cancel
+                    </button>
                 </div>
             </div>
         </div>
@@ -20,10 +138,11 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import { useClientsStore } from '../clientsStore';
-
-
-
+import { ref } from 'vue';
 
 const clientsStore = useClientsStore()
 const { clientDetails } = storeToRefs(clientsStore)
+
+
+const isEditing = ref<boolean>(false)
 </script>
